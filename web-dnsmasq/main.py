f78b3df77af2ee3f2905ac500ca31fb86b411d5e
Spudwick <olivert321@gmail.com>
2019-07-15 23:03:18 +0100

    Continued PyDNSMasq dev

--- web-dnsmasq/main.py
+++ web-dnsmasq/main.py
@@ -1,11 +1,12 @@
-import dnsmasq
+import PyDNSMasq
 
 #dnsmasq_conf = dnsmasq.config(conf_file='dnsmasq/configs/test_config1.conf')
 #dnsmasq_conf = dnsmasq.config()
 
-dns_dhcp = dnsmasq.DNSMasq("dnsmasq/configs/test_config1.conf")
+dns_dhcp = PyDNSMasq.DNSMasq("PyDNSMasq/configs/test_config1.conf")
 
 print(dns_dhcp["dhcp-host"])
 
+dns_dhcp.addOption(dns_dhcp.conf_files[-1],"test-option1")
+dns_dhcp.addOption(dns_dhcp.conf_files[-1],"test-option2","hostsandstuff")
 dns_dhcp.write()
-dns_dhcp.write(dns_dhcp.conf_files[0])
