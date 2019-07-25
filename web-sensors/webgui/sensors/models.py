0a3c3bbce2f29e2cb9f1bae098e6578ebc941dfc
Spudwick <olivert321@gmail.com>
2019-07-25 07:33:42 +0100

    stuff

--- web-sensors/webgui/sensors/models.py
+++ web-sensors/webgui/sensors/models.py
@@ -1,56 +1,56 @@
+
 from django.db import models
 
 
 class Node(models.Model):
-	node_id = models.IntegerField(default=0, unique=True)
-	node_ip = models.GenericIPAddressField(protocol="IPv4")
+    class Meta:
+        verbose_name_plural = "Nodes"
+        db_table = "nodes"
 
-	hasTmp = models.BooleanField(default=False)
-	hasHum = models.BooleanField(default=False)
-	hasLgt = models.BooleanField(default=False)
-	hasSnd = models.BooleanField(default=False)
-	hasVlt = models.BooleanField(default=False)
-	hasAmp = models.BooleanField(default=False)
+    node_id = models.IntegerField(default=0, unique=True)
+    node_ip = models.GenericIPAddressField(protocol="IPv4")
 
-	def __str__(self):
-		return "Node" + str(self.node_id)
+    hasTmp = models.BooleanField(default=False)
+    hasHum = models.BooleanField(default=False)
+    hasLgt = models.BooleanField(default=False)
+    hasSnd = models.BooleanField(default=False)
+    hasVlt = models.BooleanField(default=False)
+    hasAmp = models.BooleanField(default=False)
 
-	class Meta:
-		verbose_name_plural = "Nodes"
-		db_table = "nodes"
+    def __str__(self):
+        return "Node" + str(self.node_id)
 
 
 class NodeReading(models.Model):
-	class Meta:
-		verbose_name_plural = "Node Readings"
-		db_table = "node_readings"
+    class Meta:
+        verbose_name_plural = "Node Readings"
+        db_table = "node_readings"
 
-	timestamp = models.DateTimeField(auto_now_add=True)
-	node = models.ForeignKey(Node, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
 
-	temperature = models.IntegerField(default=0)
-	humidity = models.IntegerField(default=0)
-	light = models.IntegerField(default=0)
-	sound = models.IntegerField(default=0)
-	voltage = models.IntegerField(default=0)
-	current = models.IntegerField(default=0)
+    temperature = models.IntegerField(default=0)
+    humidity = models.IntegerField(default=0)
+    light = models.IntegerField(default=0)
+    sound = models.IntegerField(default=0)
+    voltage = models.IntegerField(default=0)
+    current = models.IntegerField(default=0)
 
-	def __str__(self):
-		return str(self.timestamp) + "-Node" + str(self.node.node_id)
+    def __str__(self):
+        return str(self.timestamp) + "-Node" + str(self.node.node_id)
 
-	def save(self, *args, **kwargs):
-		if not self.node.hasTmp:
-			self.temperature = 0
-		if not self.node.hasHum:
-			self.humidity = 0
-		if not self.node.hasLgt:
-			self.light = 0
-		if not self.node.hasSnd:
-			self.sound = 0
-		if not self.node.hasVlt:
-			self.voltage = 0
-		if not self.node.hasAmp:
-			self.current = 0
+    def save(self, *args, **kwargs):
+        if not self.node.hasTmp:
+            self.temperature = 0
+        if not self.node.hasHum:
+            self.humidity = 0
+        if not self.node.hasLgt:
+            self.light = 0
+        if not self.node.hasSnd:
+            self.sound = 0
+        if not self.node.hasVlt:
+            self.voltage = 0
+        if not self.node.hasAmp:
+            self.current = 0
 
-		super().save(*args, **kwargs)
-	
+        super().save(*args, **kwargs)
