bda6cc7caa5b14b318444d251e36cbf03d0d3fed
Spudwick <olivert321@gmail.com>
2019-07-25 22:52:25 +0100

    Web stuff

--- web-sensors/webgui/sensors/views.py
+++ web-sensors/webgui/sensors/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Node, NodeReading
+
+
 def index(request):
-	array = range(0,10)
-	return render(request, 'sensors/index.html', { 'test': "hello", 'charts': array })
+	nodes = Node.objects.all()[:5]
+
+	data = []
+	for node in nodes:
+		data.append({ "node" : node, "readings" : NodeReading.objects.filter(node=node) })
+
+	return render(request, 'sensors/index.html', {'data': data})
