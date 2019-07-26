52482aed7d68353826a15690de31e499aec0897e
Spudwick <32262684+Spudwick@users.noreply.github.com>
2019-07-26 16:26:47 +0100

    Better chart generation
    Better method to generate the chart data.

--- web-sensors/webgui/sensors/models.py
+++ web-sensors/webgui/sensors/models.py
@@ -27,47 +27,8 @@ class Node(models.Model):
 
 		super().save(*args, **kwargs)
 
-	def getTmps(self):
-		if self.hasTmp:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.temperature } for entry in entries ]
-		else:
-			return None
-
-	def getHums(self):
-		if self.hasHum:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.humidity } for entry in entries ]
-		else:
-			return None
-
-	def getLgts(self):
-		if self.hasLgt:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.light } for entry in entries ]
-		else:
-			return None
-
-	def getSnds(self):
-		if self.hasSnd:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.sound } for entry in entries ]
-		else:
-			return None
-
-	def getVlts(self):
-		if self.hasVlt:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.voltage } for entry in entries ]
-		else:
-			return None
-
-	def getAmps(self):
-		if self.hasAmp:
-			entries = NodeReading.objects.filter(node=self)
-			return [ { "x" : entry.timestamp.strftime("%Y-%m-%dT%H:%M") + "+00:00", "y" : entry.current } for entry in entries ]
-		else:
-			return None
+	def jsCharts(self, depth=None, period=None):
+		return NodeReading.jsCharts(node=self, depth=depth, period=period)
 
 
 class NodeReading(models.Model):
@@ -85,8 +46,75 @@ class NodeReading(models.Model):
 	voltage = models.IntegerField(default=0)
 	current = models.IntegerField(default=0)
 
+	@property
+	def js_timestamp(self):
+		return self.timestamp.strftime("%Y-%m-%dT%H:%M")
+
+	@classmethod
+	def searchNode(cls, node, depth=None, period=None):
+		array = cls.objects.filter(node=node).order_by('-timestamp')
+		
+		if not depth == None:
+			return array[:depth]
+		
+		elif not period == None:
+			if period[0] > period[1]:
+				raise ValueError("Reading Search : Start time more recent than End time.")
+
+			start_idx = None
+			end_idx = None
+			
+			for idx,entry in enumerate(array):
+				if entry.timestamp <= period[1]:
+					start_idx = idx
+					break
+			if not start_idx == None:
+				for idx,entry in enumerate(array[start_idx:]):
+					if entry.timestamp <= period[0]:
+						end_idx = start_idx + idx
+						break
+			else:
+				return []
+
+			if end_idx == None:
+				end_idx == len(array) - 1
+
+			return array[start_idx:end_idx]
+		
+		else:
+			return array
+
+	@classmethod
+	def jsCharts(cls, node, depth=None, period=None):
+		db = cls.searchNode(node=node, depth=depth, period=period)
+
+		tmp_set = []
+		hum_set = []
+		lgt_set = []
+		snd_set = []
+		vlt_set = []
+		amp_set = []
+
+		for entry in db:
+			tmp_set.append( { "t" : entry.js_timestamp, "y" : entry.temperature } )
+			hum_set.append( { "t" : entry.js_timestamp, "y" : entry.humidity } )
+			lgt_set.append( { "t" : entry.js_timestamp, "y" : entry.light } )
+			snd_set.append( { "t" : entry.js_timestamp, "y" : entry.sound } )
+			vlt_set.append( { "t" : entry.js_timestamp, "y" : entry.voltage } )
+			amp_set.append( { "t" : entry.js_timestamp, "y" : entry.current } )
+
+		charts = []
+		if node.hasTmp:		charts.append({ "name" : "Temperature",	"color" : [255,	153,51],	"set" : tmp_set })
+		if node.hasHum:		charts.append({ "name" : "Humidity",	"color" : [0,	0,	255],	"set" : hum_set })
+		if node.hasLgt:		charts.append({ "name" : "Light",		"color" : [255,	255,51],	"set" : lgt_set })
+		if node.hasSnd:		charts.append({ "name" : "Sound",		"color" : [0,	0,	0],		"set" : snd_set })
+		if node.hasVlt:		charts.append({ "name" : "Voltage",		"color" : [255,	0,	0],		"set" : vlt_set })
+		if node.hasAmp:		charts.append({ "name" : "Current",		"color" : [255,	51,	255],	"set" : amp_set })
+		
+		return charts
+
 	def __str__(self):
-		return str(self.timestamp) + "-Node" + str(self.node.node_id)
+		return str(self.js_timestamp) + "-Node" + str(self.node.node_id)
 
 	def save(self, *args, **kwargs):
 		if not self.node.hasTmp:
