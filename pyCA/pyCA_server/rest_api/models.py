9c75eeb69294346d86350a75ed7bb6ff20391550
Spudwick <olivert321@gmail.com>
2020-03-11 09:57:21 +0000

    Complete messages example

--- pyCA/pyCA_server/rest_api/models.py
+++ pyCA/pyCA_server/rest_api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Message(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE)
+
 	message = models.CharField(max_length=100, blank=True, default='')
 
 	class Meta:
--- pyCA/pyCA_server/rest_api/serializers.py
+++ pyCA/pyCA_server/rest_api/serializers.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_api.models import Message
 
 
-class MessageSerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.ModelSerializer):	
+	owner = serializers.ReadOnlyField(source='owner.username')
+	
 	class Meta:
 		model = Message
-		fields = ['id', 'message']
+		fields = ['id', 'created', 'owner', 'message']
+
+
+class UserSerializer(serializers.ModelSerializer):
+	messages = serializers.PrimaryKeyRelatedField(many=True, queryset=Message.objects.all())
+
+	class Meta:
+		model = User
+		fields = ['id', 'username', 'messages']
